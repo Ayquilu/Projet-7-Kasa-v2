ad491778b06496d947b9c155a36eabea65d7ce38
Lucas Carpentier <luccarpentier20@gmail.com>
2023-12-09 16:39:53 +0100

    transformation de la Home-banner en composant et debut de l'implementation des cartes de la page Home

---
+++ src/composant/Home-banner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home-banner.scss";
+
+function HomeBanner() {
+  return (
+    <div className="banner">
+      <h2>Chez vous, partout et ailleurs</h2>
+    </div>
+   
+   
+  );
+}
+
+export default HomeBanner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,36 @@
 import React from "react";
 import "./Home.scss";
+import Banner from "../composant/Home-banner.jsx";
 
 function Home() {
   return (
-    <div className="banner">
-      <h2>Chez vous, partout et ailleurs</h2>
-    </div>
+    <>
+      <Banner />
+
+      <div className="Card_container">
+        <div className="Card">
+          appartement super cool
+        </div>
+        <div className="Card">
+          appartement super cool
+        </div>
+        <div className="Card">
+          appartement super cool
+        </div>
+        <div className="Card">
+          appartement super cool
+        </div>
+        <div className="Card">
+          appartement super cool
+        </div>
+        <div className="Card">
+          appartement super cool
+        </div>
+        <div className="Card">
+          appartement super cool
+        </div>
+      </div>
+    </>
   );
 }
 
