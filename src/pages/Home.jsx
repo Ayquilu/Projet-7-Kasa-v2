74226dc29af3254c5647f9db59d7308d77756b1b
Lucas Carpentier <luccarpentier20@gmail.com>
2023-12-09 22:40:44 +0100

    suppression du composant Card container et avancement du style sur le composant card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "./Home.scss";
 import HomeBanner from "../composant/Home-banner.jsx";
-import CardContainer from "../composant/Card-Container.jsx";
-
+import Card from "../composant/Card.jsx";
 function Home() {
   return (
     <>
       <HomeBanner />
-      <CardContainer />
+      <div className="card_container">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </>
   );
 }
