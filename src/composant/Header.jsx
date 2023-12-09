88cb7ab8c5a5c81d612c053b32d4e2c65470e66c
Lucas Carpentier <luccarpentier20@gmail.com>
2023-12-09 14:55:14 +0100

    Header v1

--- src/composant/Header.jsx
+++ src/composant/Header.jsx
@@ -1,12 +1,31 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./Header.scss";
 
 function Header() {
-    return (
-        <div className="header">
-        <h1>Header</h1>
+  return (
+    <div className="header">
+      <NavLink to="/">
+        <div className="logo">
+          <img src="../Assets/Logo.png" alt="logo" />
         </div>
-    );
-    }
+      </NavLink>
+      <div className="nav-links">
+        <ul>
+          <li>
+            <NavLink to="/" activeClassName="active">
+              Accueil
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" activeClassName="active">
+              À propos
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
 
 export default Header;
