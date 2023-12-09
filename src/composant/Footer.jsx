2284daba5cec213b40764234d640346cbce0d92f
Lucas Carpentier <luccarpentier20@gmail.com>
2023-12-09 15:23:43 +0100

    footer

--- src/composant/Footer.jsx
+++ src/composant/Footer.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./Footer.scss";
-
+import LogoBlack from "../Assets/Logo-black.png";
 function Footer() {
-    return (
-        <div className="footer">
-            <h1>Footer</h1>
-        </div>
-    );
+ 
+  return (
+    <div className="footer">
+      <div>
+        <img src={LogoBlack} alt="logo" width="100" />
+      </div>
+      <p> &copy; 2020 Kasa. All rights reserved </p>
+    </div>
+  );
 }
 
 export default Footer;
+
+
